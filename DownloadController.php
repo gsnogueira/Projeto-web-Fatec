f365c47e491cb53ea8892b0da070596d0bafad5c
GabrielNogueira8 <34325394+GabrielNogueira8@users.noreply.github.com>
2018-11-29 00:34:56 -0200

    Delete DownloadController.php

--- DownloadController.php
+++
@@ -1,9 +0,0 @@
-<?php
-  header('Content-Type: application/download'); /*Nos cabeçalhos contem toda a descrição do arquivo(Tipo, Disposição, e tamanho)*/
-  header('Content-Disposition: attachment; filename="Jogo.zip"');
-  header("Content-Length: " . filesize("Jogo.zip")); 
-  $fp = fopen("Jogo.zip", "r"); /*Abre o arquivo*/
-  fpassthru($fp);/*Se ocorrer um erro, fpassthru () retorna FALSE.
-  Caso contrário, fpassthru () retorna o número de caracteres lidos do manipulador e passados ​​para a saída.*/
-  fclose($fp);/* Fecha o ponteiro do arquivo aberto*/
-?>
